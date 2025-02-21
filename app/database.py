d89afbcbfd90d6ee3461fe0bc9741083c15baf54
juanmanuel <juanmanuelramoslencina@gmail.com>
2025-02-21 19:17:37 -0300

    cambio de bd se habia caido la otra

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 #DATABASE_URL = "postgresql://postgres:juanma123@localhost:1111/fast_api"
 # https://back-fastapi.onrender.com
-DATABASE_URL= "postgresql://fast_api_1nm7_user:MTgWCA0VzQ5bYdy0TdKbpUaBnnWXPdxM@dpg-csn8m95ds78s7391c46g-a.oregon-postgres.render.com/fast_api_1nm7"
+DATABASE_URL= "postgresql://tareas_rroe_user:O4iRxv7hRZD1h0lHEzMIhzZPJ7fWnqI0@dpg-cusfjg3tq21c73b45m9g-a.oregon-postgres.render.com/tareas_rroe"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
